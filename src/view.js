e3f71df29334645f6540cdcc6e58961bd9717324
Alex Nichol <unixpickle@gmail.com>
2015-12-21 13:54:09 -0500

    support scrollwheel scrolling

--- src/view.js
+++ src/view.js
@@ -19,6 +19,7 @@ function View(barPosition, content) {
   this._boundMouseMove = this._handleMouseMove.bind(this);
 
   this._registerMouseEvents();
+  this._registerWheelEvents();
   this._bar.on('scroll', this.emit.bind(this, 'scroll'));
 }
 
@@ -79,7 +80,7 @@ View.prototype._handleMouseMove = function(e) {
   var newScrollX = this._dragStartScrolledPixels - diff;
 
   var s = this.getState();
-  this._bar.setState(new State(s.getTotalPixels(), s.getVisiblePixels(), newScrollX));
+  this.setState(new State(s.getTotalPixels(), s.getVisiblePixels(), newScrollX));
   this.emit('scroll');
 
   this._bar.flash();
@@ -97,6 +98,48 @@ View.prototype._mouseEventCoordinate = function(e) {
   } else {
     return e.clientY;
   }
+};
+
+View.prototype._registerWheelEvents = function() {
+  // NOTE: combining wheel events helps performance on several browsers.
+
+  var pendingDelta = 0;
+  var secondaryDelta = 0;
+  var pendingRequest = false;
+  this._element.addEventListener('wheel', function(e) {
+    if (!pendingRequest) {
+      pendingRequest = true;
+      window.requestAnimationFrame(function() {
+        pendingRequest = false;
+
+        // NOTE: when you scroll vertically on a trackpad on OS X,
+        // it unwantedly scrolls horizontally by a slight amount.
+        if (Math.abs(secondaryDelta) > 2*Math.abs(pendingDelta)) {
+          pendingDelta = 0;
+          secondaryDelta = 0;
+          return;
+        }
+
+        var state = this.getState();
+        this.setState(new State(state.getTotalPixels(), state.getVisiblePixels(),
+          state.getScrolledPixels() + pendingDelta));
+        this.emit('scroll');
+
+        pendingDelta = 0;
+        secondaryDelta = 0;
+
+        this._bar.flash();
+      }.bind(this));
+    }
+    if (this._bar.getOrientation() === Bar.ORIENTATION_HORIZONTAL) {
+      pendingDelta += e.deltaX;
+      secondaryDelta += e.deltaY;
+    } else {
+      pendingDelta += e.deltaY;
+      secondaryDelta += e.deltaX;
+    }
+    e.preventDefault();
+  }.bind(this));
 };
 
 exports.View = View;
