fd9b8581ff793d40a2a70d54a3fe16fc89e38fd8
Alex Nichol <unixpickle@gmail.com>
2015-12-21 16:43:19 -0500

    added vertical scrolling demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function() {
   addHorizontalCanvas();
+  addVerticalCanvas();
 });
 
 function addHorizontalCanvas() {
@@ -17,7 +18,8 @@ function addHorizontalCanvas() {
   v.element().style.width = '100%';
   v.element().style.height = '300px';
   document.body.appendChild(v.element());
-  v.setState(new window.scrollerjs.State(2000, v.element().offsetWidth, 2000-400));
+  v.setState(new window.scrollerjs.State(2000, v.element().offsetWidth,
+    2000-v.element().offsetWidth));
   v.layout();
   v.setDraggable(true);
 
@@ -44,6 +46,48 @@ function addHorizontalCanvas() {
   });
 
   canvas.width = v.element().offsetWidth;
+
+  v.on('scroll', drawCanvas);
+  drawCanvas();
+}
+
+function addVerticalCanvas() {
+  var canvas = document.createElement('canvas');
+  canvas.style.position = 'absolute';
+  canvas.style.width = '100%';
+  canvas.style.height = '100%';
+  canvas.style.top = '0';
+  canvas.style.left = '0';
+  canvas.style.backgroundColor = 'white';
+  canvas.height = 300;
+  canvas.width = 200;
+
+  var v = new window.scrollerjs.View(window.scrollerjs.View.BAR_POSITION_RIGHT,
+    canvas);
+  v.element().style.width = '200px';
+  v.element().style.height = '300px';
+  v.element().style.left = 'calc(50% - 100px)';
+  v.element().style.top = '20px';
+  v.element().style.marginBottom = '40px';
+  document.body.appendChild(v.element());
+  v.setState(new window.scrollerjs.State(2000, 300, 2000-300));
+  v.layout();
+  v.setDraggable(true);
+
+  var drawCanvas = function() {
+    var offset = v.getState().getScrolledPixels();
+    var ctx = canvas.getContext('2d');
+    ctx.save();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.translate(0, -offset);
+    ctx.fillStyle = '#65bcd4';
+    for (var i = 0; i < 50; ++i) {
+      var y = i*40 + 5;
+      var width = (123*i*i + 120*i + 15*i*i*i + 6) % 150;
+      ctx.fillRect(5, y, width, 30);
+    }
+    ctx.restore();
+  };
 
   v.on('scroll', drawCanvas);
   drawCanvas();
