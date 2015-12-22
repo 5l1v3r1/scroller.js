1bb6d38ca2b7cd866901fdc2bd8805fd8cf0547e
Alex Nichol <unixpickle@gmail.com>
2015-12-22 04:39:29 +0000

    smoother demo canvas on retina displays

--- demo/demo.js
+++ demo/demo.js
@@ -11,7 +11,7 @@ function addHorizontalCanvas() {
   canvas.style.top = '0';
   canvas.style.left = '0';
   canvas.style.backgroundColor = 'white';
-  canvas.height = 300;
+  canvas.height = 600;
 
   var v = new window.scrollerjs.View(window.scrollerjs.View.BAR_POSITION_BOTTOM,
     canvas);
@@ -28,24 +28,25 @@ function addHorizontalCanvas() {
     var ctx = canvas.getContext('2d');
     ctx.save();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.scale(2, 2);
     ctx.translate(-offset, 0);
     ctx.fillStyle = '#65bcd4';
     for (var i = 0; i < 50; ++i) {
       var x = i*40 + 5;
       var height = (123*i*i + 120*i + 15*i*i*i + 6) % 257;
-      ctx.fillRect(x, canvas.height-5-height, 30, height);
+      ctx.fillRect(x, 300-5-height, 30, height);
     }
     ctx.restore();
   };
 
   window.addEventListener('resize', function() {
-    canvas.width = v.element().offsetWidth;
+    canvas.width = v.element().offsetWidth * 2;
     v.setState(new window.scrollerjs.State(2000, v.element().offsetWidth,
       v.getState().getScrolledPixels()));
     drawCanvas();
   });
 
-  canvas.width = v.element().offsetWidth;
+  canvas.width = v.element().offsetWidth * 2;
 
   v.on('scroll', drawCanvas);
   drawCanvas();
@@ -61,8 +62,8 @@ function addVerticalCanvas() {
   canvas.style.top = '0';
   canvas.style.left = '0';
   canvas.style.backgroundColor = 'white';
-  canvas.height = 300;
-  canvas.width = 200;
+  canvas.height = 600;
+  canvas.width = 400;
 
   var v = new window.scrollerjs.View(window.scrollerjs.View.BAR_POSITION_RIGHT,
     canvas);
@@ -81,6 +82,7 @@ function addVerticalCanvas() {
     var ctx = canvas.getContext('2d');
     ctx.save();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.scale(2, 2);
     ctx.translate(0, -offset);
     ctx.fillStyle = '#65bcd4';
     for (var i = 0; i < 50; ++i) {
